1b5d56b98c19c87e4825ba6bcec6b8b3f96515b5
Katya Weicht <katya.weicht@gmail.com>
2025-07-10 19:08:42 -0600

    validate that data is a list and update logic in main

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,35 +1,29 @@
 from datetime import datetime
-import os
 import requests
 
 def generate_log(data):
-    # TODO: Implement log generation logic
-
-    # STEP 1: Validate input
-    # Hint: Check if data is a list
-
-    # STEP 2: Generate a filename with today's date (e.g., "log_20250408.txt")
-    # Hint: Use datetime.now().strftime("%Y%m%d")
-
-    # STEP 3: Write the log entries to a file using File I/O
-    # Use a with open() block and write each line from the data list
-    # Example: file.write(f"{entry}\n")
-
-    # STEP 4: Print a confirmation message with the filename
-
-    log_data = ["User logged in", "User updated profile", "Report exported"]
+    if isinstance(data,list):
+        log_data = data
+    else:
+        raise ValueError
+   
     filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
     with open(filename, "w") as file:
         for entry in log_data:
             file.write(f"{entry}\n")
     print(f"Log written to {filename}")
+    return filename
 
 def fetch_data():
-    response = response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    response = response = requests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
         return response.json()
     return {}
 
 if __name__ == "__main__":
-    post = fetch_data()
-    print("Fetched Post Title", post.get("title", "No title found"))
+    posts = fetch_data()
+    data = []
+    for post in posts:
+       data.append(post.get("title"))
+    generate_log(data)
+   
